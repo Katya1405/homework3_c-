7b96bc03ba3953f8aecbbf129b3baf8efa6f8d21
Katya <bozhkova-ekaterina@mail.ru>
2022-10-08 15:38:15 +0300

    Добавили решение задачи 3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int A = 1;
+int avg = 1;
+while (A <= N)
+{
+    avg = avg * A;
+    A++;
+}
+Console.WriteLine(avg);
