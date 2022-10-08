f6a008df60e63a501a6b5de689bf7a12b21d6954
Katya <bozhkova-ekaterina@mail.ru>
2022-10-08 15:49:42 +0300

    добавили решение задачи 4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int number = 0;
+while (number <= N)
+{
+    if (number % 2 == 0)
+    {
+        double result = Math.Pow(number, 3);
+        Console.WriteLine("Куб числа " + number + " = " + result);
+    }
+    number++;
+}
